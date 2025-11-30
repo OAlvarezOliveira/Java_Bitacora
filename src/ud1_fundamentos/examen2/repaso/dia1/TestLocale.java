23781ef6e70786fdacf0205d5911450dd913c5b9
OAlvarezOliveira <oalvarezoliveira@gmail.com>
2025-11-30 01:23:20 +0100

    Fin repaso dia 1- 29/11

---
+++ src/ud1_fundamentos/examen2/repaso/dia1/TestLocale.java
@@ -0,0 +1,9 @@
+package ud1_fundamentos.examen2.repaso.dia1;
+
+import java.util.Locale;
+
+public class TestLocale {
+    public static void main(String[] args) {
+        System.out.println(Locale.getDefault());
+    }
+}
